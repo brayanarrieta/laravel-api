dc4d3e3b7c500142aed43fe5537f8e8c1ba2c372
Brayan Arrieta Alfaro <brayan.arrieta.alfaro.96@gmail.com>
2018-02-18 12:56:59 -0600

    Country controller

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Country;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return void
+     */
+    public function index()
+    {
+        $countries =Country::all();
+        return response()->json($countries,200);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return void
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Country::create($request->all());
+        return response()->json(["message" => "The Country was created."]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $country=Country::find($id);
+        return response()->json($country);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return void
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $country = Country::find($id);
+        $country->fill($request->all());
+        $country->save();
+        return response()->json(["message" => "The Country was updated."]);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return void
+     */
+    public function destroy($id)
+    {
+        $country = Country::find($id);
+        $country->delete();
+        return response()->json(["message" => "The Country was deleted."]);
+    }
+}
